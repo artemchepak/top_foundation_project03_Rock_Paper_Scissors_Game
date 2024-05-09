f5148d60dc06e3a1bf1d609bf2664eef71653f39
Artem Chepak <a.chepak91@gmail.com>
2024-05-09 11:45:04 -0600

    remove log from getHumanChoice function

--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,23 @@
 const choiseOptions = ['rock', 'paper', 'scissors'];
+// item in 1st array in winningCombinations beats item in 2nd array on the same possition (key)
+// winningCombinations[0][0] beats winningCombinations[1][0] 
+const winningCombinations = [['rock', 'paper', 'scissors'], ['scissors', 'rock', 'paper']];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoise = choiseOptions[Math.floor(Math.random() * choiseOptions.length)];
-    return computerChoise;
+    let computerChoice = choiseOptions[Math.floor(Math.random() * choiseOptions.length)];
+    return computerChoice;
 }
 
 function getHumanChoice() {
     let humanInput = prompt('Enter your choise. Valid options are: Rock, Paper, Scissors');
-    let humanChoise = validateHumanChoise(humanInput);
-    return humanChoise;
+    let humanChoice = validateHumanChoise(humanInput);
+    return humanChoice;
 }
 
 function validateHumanChoise(str) {
     let result = str.trim().toLowerCase();
-    console.log(str + ' ' + result);
 
     if (choiseOptions.includes(result)) {
         return result;
@@ -22,3 +26,39 @@ function validateHumanChoise(str) {
         getHumanChoice();
     }
 }
+
+function getWinner(human, computer) {
+    let humanArrayIndex = winningCombinations[0].indexOf(human);
+    let computerArrayIndex = winningCombinations[1].indexOf(computer);
+    if (humanArrayIndex === computerArrayIndex) {
+        return 'human';
+    }
+    else {
+        return 'computer';
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You choose ${humanChoice}, Computer coose ${computerChoice}`)
+
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+
+    } else {
+        let winner = getWinner(humanChoice, computerChoice);
+
+        if (winner = 'computer') {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1;
+        }
+        else {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 0;
+        }
+    }
+
+    console.log(`Game score: Player: ${humanScore}, Computer: ${computerScore}`)
+}
+
+playRound(getHumanChoice(), getComputerChoice());
