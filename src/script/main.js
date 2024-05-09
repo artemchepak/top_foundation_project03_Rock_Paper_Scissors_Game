d0b6d56b0a38bb7810d8a67f341c7f894ef29aaf
Artem Chepak <a.chepak91@gmail.com>
2024-05-09 11:43:20 -0600

    add getHumanChoice function and input validation

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,24 @@
+const choiseOptions = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    const choiseOptions = ['Rock', 'Paper', 'Scissors'];
     let computerChoise = choiseOptions[Math.floor(Math.random() * choiseOptions.length)];
     return computerChoise;
+}
+
+function getHumanChoice() {
+    let humanInput = prompt('Enter your choise. Valid options are: Rock, Paper, Scissors');
+    let humanChoise = validateHumanChoise(humanInput);
+    return humanChoise;
+}
+
+function validateHumanChoise(str) {
+    let result = str.trim().toLowerCase();
+    console.log(str + ' ' + result);
+
+    if (choiseOptions.includes(result)) {
+        return result;
+    }
+    else {
+        getHumanChoice();
+    }
 }
